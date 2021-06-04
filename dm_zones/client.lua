1e2ed9fcf048e2d40ce4f3ac1d83f662338a9871
^Dm <84171471+Dm-CoDeR-2021@users.noreply.github.com>
2021-06-04 21:09:10 +0430

    Update client.lua

--- dm_zones/client.lua
+++ dm_zones/client.lua
@@ -8,10 +8,17 @@ Citizen.CreateThread(function()
   while true do
     Wait(0)
     for k, v in pairs(Config.locations) do
-      zone = AddBlipForRadius(v.pos.x, v.pos.y, v.pos.z, 1000.0)
-      SetBlipSprite(zone, 1)
-      SetBlipAlpha(zone, 1)
+      local alpha = 250
+      zone = AddBlipForRadius(v.pos.x, v.pos.y, v.pos.z, 130.0)
+      SetBlipAsShortRange(zone, true)
+      SetBlipHighDetail(zone, true)
+      SetBlipAlpha(zone, alpha)
       SetBlipColour(zone, 1)
+      while alpha ~= 0 do
+			  Citizen.Wait(30000)
+			  alpha = alpha - 1
+			  SetBlipAlpha(mzone, alpha)
+      end
     end
   end
 end)
